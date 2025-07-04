0f1acc4c2397080c85477d3b1077f243973e8210
Luis Vega <vegaluisfernando2018@gmail.com>
2025-07-04 15:29:48 -0300

    Cambios importantes

---
+++ script.js
@@ -0,0 +1,67 @@
+function calcularSueldo() {
+      const categoria = document.getElementById("categoria").value;
+      const sindicato = document.getElementById("sindicato").checked;
+      const antiguedadinput = parseFloat(document.getElementById("antiguedad").value || 0);
+      const basico = parseFloat(document.getElementById("basico").value) || 0;
+      const horasnoc = parseFloat(document.getElementById("hsnoc").value) || 0;
+      const hs50 = parseFloat(document.getElementById("hs50").value) || 0;
+      const hs100 = parseFloat(document.getElementById("hs100").value) || 0;
+      const feriados = parseFloat(document.getElementById("feriados").value) || 0;
+
+      // validacion de categoria
+      let adicional
+      if (categoria == "monitoreo") {
+        adicional = 49582.49
+      } else if (categoria == "encargado") {
+        adicional = 98000
+      } else {
+        adicional = 0
+      }
+
+      // Calcuca
+      const antiguedad = antiguedadinput / 100
+      const nocturnas = horasnoc * (702.22 + (702.22 * antiguedad)) ;
+      const totalHs50 = hs50 * (6487.28 + (6487.28 * antiguedad));
+      const totalHs100 = hs100 * (8649.70 + (8649.70 * antiguedad)); 
+      const totalant = basico * antiguedad;
+      const NoRem = 429750;
+      const Presentismo = 153600;
+      const totalFeriado = (basico + Presentismo + 26280) / 25 * feriados;
+      const totalA = basico + nocturnas + totalHs50 + totalHs100 + totalant + totalFeriado + Presentismo + adicional + 26280;
+
+      // Descuentos
+      const jubilacion = totalA * 0.11;
+      const ley19032 = totalA * 0.03;
+      const obrasocial = totalA * 0.03;
+      const descsindi = totalA * 0.03 + totalA * 0.01;
+      const totalDescuento = jubilacion + ley19032 + obrasocial;
+      //Validacion de sindicato.
+      let total
+      if (sindicato) {
+        total = totalA + NoRem - totalDescuento - descsindi;
+      } else {
+        total = totalA + NoRem - totalDescuento;
+      }
+      // redondeo
+      
+      document.getElementById("resultado").innerHTML = `
+        <img src="https://www.informatique-mania.com/wp-content/uploads/2020/12/Meme-Faces-2.jpg" alt="imagen" style="position: absolute; opacity:0.2; width: 100%; height: 100%;object-fit: cover; top: 0; left: 0; z-index: 0;">
+        <div style="margin: 20px; position: relative;z-index: 1">
+        <p>Sueldo Básico: $${basico.toFixed(2)}</p>
+        <p>Horas nocturnas: $${nocturnas.toFixed(2)}</p>
+        <p>Horas 50%: $${totalHs50.toFixed(2)}</p>
+        <p>Horas 100%: $${totalHs100.toFixed(2)}</p>
+        <p>Adicional REM Seguridad: $26.280</p>
+        <p>AD por Presentismo: $${Presentismo}</p>
+        <p>Plus categoria: $${adicional}</p>
+        <p>Feriados Trabajados: $${totalFeriado.toFixed(2)}</p>
+        <p>Total S/Viatico.: $${totalA.toFixed(2)}</p>
+        <p><strong>Descuentos</strong></p>
+        <p>Jubilacion: $${jubilacion.toFixed(2)}</p>
+        <p>Ley19032: $${ley19032.toFixed(2)}</p>
+        <p>Obra Social: $${obrasocial.toFixed(2)}</p>
+        <p>Descuentos sindicato y Cuota de solidaridad: $${descsindi.toFixed(2)}</p>
+        <p><strong>Total a cobrar: $${total.toFixed(2)}</strong></p>
+        </div>
+      `;
+    }
